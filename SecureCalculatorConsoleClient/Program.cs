d4d76d353535a55e2e44147d0fa579d3b290b92e
Igor Martins-WDG <igor.martins-wdg@ibm.com>
2023-04-11 20:15:09 -0300

    Add project files.

---
+++ SecureCalculatorConsoleClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using SecureCalculatorConsoleClient.MyServiceReference;
+using System;
+
+namespace SecureCalculatorConsoleClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var client = new CalculatorClient();
+
+                //If you want to manually specify windows user!
+                //client.ClientCredentials.Windows.ClientCredential.UserName = "admin";
+                //client.ClientCredentials.Windows.ClientCredential.Password = "something";
+
+                var total = client.Add(1, 3);
+                Console.WriteLine($"Total: {total}");
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
